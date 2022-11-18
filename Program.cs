080fe21e265cbdaa42c73121ceccec52e5ab73ff
Thomas Castiglione <thomas@agilepc.com>
2022-11-18 14:29:51 +0800

    create project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using The.Persistence;
+using The.Security;
+using Repro.Model;
+
+var services = new The.Composition.Builder()
+    .RegisterMetadata(Entities.Metadata)
+    .RegisterEntityStore(new TransientEntityStore(Entities.Metadata, MemoryPersistence.CreatePersisters()))
+    .RegisterAuthentication(new PresetAuthentication("test"))
+    .Build();
+
+Console.WriteLine($"Hello, {services.AppName}!");
